2701c710545315f64c367c4a687ff367cdb83300
dgtlmoon <dgtlmoon@gmail.com>
2026-02-03 15:31:20 +0100

    Better handling of settings/edit tabs available with changedetection.io

--- changedetectionio_osint/__init__.py
+++ changedetectionio_osint/__init__.py
@@ -12,3 +12,10 @@ description = _('Comprehensive reconnaissance using OSINT tools (DNS, WHOIS, SSL
 processor_weight = -50  # Show before text_json_diff
 list_badge_text = "OSINT"
 del _
+
+# Processor capabilities (defaults to False unless specified)
+supports_visual_selector = False
+supports_browser_steps = False
+supports_text_filters_and_triggers = True
+supports_request_type = False
+
